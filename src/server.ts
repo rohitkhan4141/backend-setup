c39fd41051f3798f045bc2014f13d324893ef7b1
rohitkhan4141 <rohitkhan4141@gmail,com>
2023-08-23 10:30:22 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+async function main() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    console.log(`database is connected successfully`);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(`problem facing on connecting database, ${err}`);
+  }
+}
+
+main();
