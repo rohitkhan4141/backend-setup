e862a41c7312f8c68e705b186492ec4dee1a8d61
rohitkhan4141 <rohitkhan4141@gmail,com>
2023-08-23 12:05:02 +0600

    larelappa

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-import express, { Application } from "express";
+import express, { Application } from 'express';
 const app: Application = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
 export default app;
