f199eef33c35677fc077195346c4b8b2d6ef4c3b
rohitkhan4141 <rohitkhan4141@gmail,com>
2023-08-23 12:06:20 +0600

    finale push

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 
 app.get('/', (req: Request, res: Response) => {
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
 
 async function main() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
     console.log(`database is connected successfully`);
     app.listen(config.port, () => {
       console.log(`Example app listening on port ${config.port}`);
